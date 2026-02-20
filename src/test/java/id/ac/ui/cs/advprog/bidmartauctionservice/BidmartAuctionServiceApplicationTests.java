d0550da97f5d8430a9a0dc0e8140c7a18a8107ad
aldofahrezy <aldofhrzy@gmail.com>
2026-02-20 10:21:26 +0700

    test: ensure contextLoads passes with h2 configuration

--- src/test/java/id/ac/ui/cs/advprog/bidmartauctionservice/BidmartAuctionServiceApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/bidmartauctionservice/BidmartAuctionServiceApplicationTests.java
@@ -2,12 +2,14 @@ package id.ac.ui.cs.advprog.bidmartauctionservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class BidmartAuctionServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
