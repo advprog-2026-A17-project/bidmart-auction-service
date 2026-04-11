ceaab6c081ab42bc9b837d7b796dc02e06d07d61
aldofahrezy <aldofhrzy@gmail.com>
2026-04-11 19:35:18 +0700

    refactor: Fix ApplicationEvent constructors for events

--- src/main/java/id/ac/ui/cs/advprog/bidmartauctionservice/event/AuctionEndedEvent.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmartauctionservice/event/AuctionEndedEvent.java
@@ -2,12 +2,10 @@ package id.ac.ui.cs.advprog.bidmartauctionservice.event;
 
 import id.ac.ui.cs.advprog.bidmartauctionservice.model.entity.Auction;
 import id.ac.ui.cs.advprog.bidmartauctionservice.model.enums.AuctionStatus;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 @Getter
-@AllArgsConstructor
 public class AuctionEndedEvent extends ApplicationEvent {
     private final Auction auction;
     private final AuctionStatus finalStatus;
--- src/main/java/id/ac/ui/cs/advprog/bidmartauctionservice/event/BidPlacedEvent.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmartauctionservice/event/BidPlacedEvent.java
@@ -1,12 +1,10 @@
 package id.ac.ui.cs.advprog.bidmartauctionservice.event;
 
 import id.ac.ui.cs.advprog.bidmartauctionservice.model.entity.Bid;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 @Getter
-@AllArgsConstructor
 public class BidPlacedEvent extends ApplicationEvent {
     private final Bid bid;
 
