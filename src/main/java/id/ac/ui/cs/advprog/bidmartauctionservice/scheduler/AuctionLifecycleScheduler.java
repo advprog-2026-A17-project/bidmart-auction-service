3dd4a99d7f52c349910cfa846b1f609f8ee8921e
aldofahrezy <aldofhrzy@gmail.com>
2026-04-11 19:30:37 +0700

    test: [green] Implement event publishing for BidPlaced and AuctionEnded

--- src/main/java/id/ac/ui/cs/advprog/bidmartauctionservice/scheduler/AuctionLifecycleScheduler.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmartauctionservice/scheduler/AuctionLifecycleScheduler.java
@@ -1,9 +1,11 @@
 package id.ac.ui.cs.advprog.bidmartauctionservice.scheduler;
 
+import id.ac.ui.cs.advprog.bidmartauctionservice.event.AuctionEndedEvent;
 import id.ac.ui.cs.advprog.bidmartauctionservice.model.entity.Auction;
 import id.ac.ui.cs.advprog.bidmartauctionservice.model.enums.AuctionStatus;
 import id.ac.ui.cs.advprog.bidmartauctionservice.repository.AuctionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,7 @@ import java.util.List;
 public class AuctionLifecycleScheduler {
 
     private final AuctionRepository auctionRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Scheduled(fixedDelay = 30000) // Run every 30 seconds
     @Transactional
@@ -28,13 +31,16 @@ public class AuctionLifecycleScheduler {
         List<Auction> expiredAuctions = auctionRepository.findEndedAuctionsByMultipleStatuses(statuses, now);
 
         for (Auction auction : expiredAuctions) {
+            AuctionStatus finalStatus;
             if (auction.getCurrentHighestBid() != null &&
                 auction.getCurrentHighestBid().compareTo(auction.getReservePrice()) >= 0) {
-                auction.setStatus(AuctionStatus.WON);
+                finalStatus = AuctionStatus.WON;
             } else {
-                auction.setStatus(AuctionStatus.UNSOLD);
+                finalStatus = AuctionStatus.UNSOLD;
             }
+            auction.setStatus(finalStatus);
             auctionRepository.save(auction);
+            eventPublisher.publishEvent(new AuctionEndedEvent(this, auction, finalStatus));
         }
     }
 }
